1efbde9be6f4e50929b6a6e40acff9f475aebc38
Andrew Pilson <andrewpilson@Mac-Computer-3.local>
2026-03-25 00:07:07 -0700

    fix: remove invalid bash comments from JS files (syntax error)

--- apify/actors/ds-govdeals/src/main.js
+++ apify/actors/ds-govdeals/src/main.js
@@ -1,5 +1,3 @@
 // Main Apify entrypoint. The working GovDeals scraper lives in src/main_api.js,
 // which handles Maestro API capture and direct pagination.
 import './main_api.js';
-# deploy Tue Mar 24 23:57:03 PDT 2026
-# Wed Mar 25 00:01:36 PDT 2026
