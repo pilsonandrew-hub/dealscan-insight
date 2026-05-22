98d4916a3fbd5b4638c8130ddb717d3cd769aa1d
Andrew Pilson <pilson.andrew@gmail.com>
2026-05-22 15:56:28 -0700

    Revert "ace: add tracked operator scope hooks"
    This reverts commit 0ae3ef3844c472c9f95010f3f2919cb1a3311096.

--- ace/hooks/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -eu
-repo_root=$(git rev-parse --show-toplevel)
-cd "$repo_root"
-git config core.hooksPath ace/hooks
-printf 'ACE operator-scope hooks activated for this clone via core.hooksPath=ace/hooks\n'
